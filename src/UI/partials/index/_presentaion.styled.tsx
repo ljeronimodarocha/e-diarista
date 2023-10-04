2d6bc8b302be23f5c2a67ef3444847575118d9d1
ljeronimodarocha <ljeronimodarocha@gmail.com>
2023-10-04 14:21:59 -0300

    9º semana, 20º aula.

--- src/UI/partials/index/_presentaion.styled.tsx
+++ src/UI/partials/index/_presentaion.styled.tsx
@@ -1,4 +1,4 @@
-import { styled } from "@mui/material";
+import { Container, styled } from "@mui/material";
 
 export const SectionContainer = styled("section")`
   min-height: 250px;
@@ -14,5 +14,29 @@ export const SectionContainer = styled("section")`
 
   ${({ theme }) => theme.breakpoints.up("md")} {
     background-position: center;
+  }
+`;
+
+export const ContainerStyled = styled(Container)`
+  display: grid;
+  grid-template-rows: repeat(3, auto);
+  grid-template-areas: "title" "subtitle" "button";
+  align-content: center;
+
+  ${({ theme }) => theme.breakpoints.up("md")} {
+    grid-template-columns: 450px minmax(200px, 450px);
+    grid-template-rows: 125px, 55px 60px;
+    grid-template-areas:
+      "title picture"
+      "subtitle picture"
+      "button picture";
+    gap: ${({ theme }) => theme.spacing(4)};
+    justify-content: space-between;
+    justify-items: center;
+    min-height: 450px;
+  }
+
+  ${({ theme }) => theme.breakpoints.down("md")} {
+    max-width: 350px;
   }
 `;
--- src/UI/partials/index/_presentaion.tsx
+++ src/UI/partials/index/_presentaion.tsx
@@ -1,7 +1,26 @@
-import { SectionContainer } from "./_presentaion.styled";
+import { ContainerStyled, SectionContainer } from "./_presentaion.styled";
 
 const Presentation: React.FC = () => {
-  return <SectionContainer></SectionContainer>;
+  return (
+    <SectionContainer>
+      <ContainerStyled>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt,
+          fugiat? Reprehenderit ipsa tempore veritatis repudiandae rerum libero.
+          Hic qui corporis quis exercitationem.
+        </div>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt,
+        </div>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt,
+        </div>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt,
+        </div>
+      </ContainerStyled>
+    </SectionContainer>
+  );
 };
 
 export default Presentation;
